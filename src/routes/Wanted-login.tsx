be207d25b3e6e76d834aa426e86c078903ad02ca
minseok1109 <park485201@naver.com>
2023-08-21 16:51:43 +0900

    feat: material ui 말고 shadcn/ui tailwindcss  적용

--- src/routes/Wanted-login.tsx
+++ src/routes/Wanted-login.tsx
@@ -1,23 +1,34 @@
-import { Box, Button, Container, CssBaseline } from "@mui/material";
-import Input from "../components/Input";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../components/ui/form";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+
 const { ipcRenderer } = window.require("electron/renderer");
-interface FormValue {
-  id?: string;
-  password?: string;
-}
+
+const formSchema = z.object({
+  id: z.string().nonempty("아이디를 입력해주세요."),
+  password: z.string().nonempty("비밀번호를 입력해주세요."),
+});
 
 const WantedLogin = () => {
-  const navigate = useNavigate();
-  const { control, handleSubmit } = useForm<FormValue>({
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       id: "",
       password: "",
     },
   });
 
-  const onSubmit = (data: FormValue) => {
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
     console.log("click");
     ipcRenderer.send("login", data);
     ipcRenderer.on("success", (event, arg) => {
@@ -27,43 +38,52 @@ const WantedLogin = () => {
   };
 
   return (
-    <>
-      <CssBaseline />
-      <Container
-        maxWidth="sm"
-        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-      >
-        <Button onClick={() => navigate(-1)}>뒤로가기</Button>
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Input
-              control={control}
-              name="id"
-              textFieldProps={{
-                label: "아이디",
-                variant: "outlined",
-                margin: "normal",
-                fullWidth: true,
-              }}
-            />
-            <Input
-              control={control}
-              name="password"
-              textFieldProps={{
-                label: "비밀번호",
-                variant: "outlined",
-                margin: "normal",
-                fullWidth: true,
-                type: "password",
-              }}
-            />
-            <Button variant="contained" type="submit" fullWidth>
-              로그인
-            </Button>
-          </form>
-        </Box>
-      </Container>
-    </>
+    <div className="w-full h-full flex justify-center items-center flex-col mx-auto">
+      <img
+        src="/Users/bangminseog/Documents/project/moapick-electron/public/wanted_logo.png"
+        alt="wanted_logo"
+      />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-96  "
+        >
+          <FormField
+            control={form.control}
+            name="id"
+            render={({ field }) => (
+              <FormItem className="space-y-1">
+                <FormLabel>아이디</FormLabel>
+                <FormControl>
+                  <Input placeholder="아이디를 입력하세요" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="space-y-1">
+                <FormLabel>비밀번호</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="비밀번호를 입력하세요"
+                    type="password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-64">
+            연동하기
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 };
 
