06c1a216c791f6b38bb5c2b8ed26568824aec4fa
studio-kyj <studio.BE.YJ@gmail.com>
2023-08-21 22:25:55 +0900

    feat : database 연동

---
+++ src/db/schemas/applicants.js
@@ -0,0 +1,14 @@
+// const applicantSchema = {
+//     type: 'object',
+//     properties: {
+//       content: {
+//         type: 'string',
+//       },
+//       isDone: {
+//         type: 'boolean',
+//         default: false
+//       }
+//     },
+//   };
+  
+//   module.exports = applicantSchema;
